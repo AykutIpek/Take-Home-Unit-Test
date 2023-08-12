18d1d20855c615f9b4fee0304b6deb0c19ca5fc5
AykutIpek <aykutipek_2001@hotmail.com>
2023-08-12 16:02:33 +0300

    Initial Commit

---
+++ iOSTakeHomeProject/iOSTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTakeHomeProjectApp.swift
+//  iOSTakeHomeProject
+//
+//  Created by aykut ipek on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
