0ed973c51ab4da585b6ad35eeacc49bfe3d37db0
AykutIpek <aykutipek_2001@hotmail.com>
2023-08-21 14:39:22 +0300

    test_alert_is_shown_when_submission_fails func written

--- iOSTakeHomeProjectUITests/Create/CreateFailureUITests.swift
+++ iOSTakeHomeProjectUITests/Create/CreateFailureUITests.swift
@@ -26,6 +26,35 @@ final class CreateFailureUITests: XCTestCase {
         app = nil
     }
     
-    
+    func test_alert_is_shown_when_submission_fails() {
+        let createBtn = app.buttons["createBtn"]
+        XCTAssertTrue(createBtn.waitForExistence(timeout: 5), "There create button should be visible on the screen")
+        
+        createBtn.tap()
+        
+        let firstNameTxtFields = app.textFields["firstNameTxtFields"]
+        let lastnameTxtFields = app.textFields["lastNameTxtFields"]
+        let jobTxtFields = app.textFields["jobTxtFields"]
+        
+        firstNameTxtFields.tap()
+        firstNameTxtFields.typeText("Aykut")
+
+        lastnameTxtFields.tap()
+        lastnameTxtFields.typeText("Ipek")
+        
+        jobTxtFields.tap()
+        jobTxtFields.typeText("iOS Developer")
+        
+        let submitBtn = app.buttons["submitBtn"]
+        XCTAssertTrue(submitBtn.exists, "There create button should be visible on the screen")
+        
+        submitBtn.tap()
+        
+        let alert = app.alerts.firstMatch
+        XCTAssertTrue(alert.waitForExistence(timeout: 5))
+        
+        XCTAssertTrue(alert.staticTexts["URL isn't valid"].exists)
+        XCTAssertTrue(alert.buttons["OK"].exists)
+    }
     
 }
