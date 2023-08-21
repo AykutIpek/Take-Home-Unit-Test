55732c0bc7cd3cb3c613ad3e6b3d012d3ce7874b
AykutIpek <aykutipek_2001@hotmail.com>
2023-08-21 14:21:31 +0300

    test_when_job_form_field_is_empty_job_error_is_shown func written

--- iOSTakeHomeProjectUITests/Create/CreateScreenFormValidationTests.swift
+++ iOSTakeHomeProjectUITests/Create/CreateScreenFormValidationTests.swift
@@ -83,7 +83,7 @@ final class CreateScreenFormValidationTests: XCTestCase {
         XCTAssertEqual(app.alerts.count, 0, "There should be no alerts on the screen")
     }
     
-    func test_when_last_name_form_field_is_empty_first_name_error_is_shown() {
+    func test_when_last_name_form_field_is_empty_last_name_error_is_shown() {
         
         let createBtn = app.buttons["createBtn"]
         XCTAssertTrue(createBtn.waitForExistence(timeout: 5), "The create button should be visible on the screen")
@@ -114,6 +114,41 @@ final class CreateScreenFormValidationTests: XCTestCase {
         alertBtn.tap()
         
         XCTAssertTrue(app.staticTexts["Last name can't be empty"].exists)
+        
+        XCTAssertEqual(app.alerts.count, 0, "There should be no alerts on the screen")
+    }
+    
+    func test_when_job_form_field_is_empty_job_error_is_shown() {
+        
+        let createBtn = app.buttons["createBtn"]
+        XCTAssertTrue(createBtn.waitForExistence(timeout: 5), "The create button should be visible on the screen")
+        
+        createBtn.tap()
+        
+        let firstNameTxtFields = app.textFields["firstNameTxtFields"]
+        let lastNameTxtFields = app.textFields["lastNameTxtFields"]
+        
+        firstNameTxtFields.tap()
+        firstNameTxtFields.typeText("Ads")
+        
+        lastNameTxtFields.tap()
+        lastNameTxtFields.typeText("Ipek")
+        
+        let submitBtn = app.buttons["submitBtn"]
+        XCTAssertTrue(submitBtn.waitForExistence(timeout: 5), "There create button should be visible on the screen")
+        
+        submitBtn.tap()
+        
+        let alert = app.alerts.firstMatch
+        let alertBtn = alert.buttons.firstMatch
+        
+        XCTAssertTrue(alert.waitForExistence(timeout: 5), "There should be an alert on the screen")
+        XCTAssertTrue(alert.staticTexts["Job can't be empty"].exists)
+        XCTAssertEqual(alertBtn.label, "OK")
+        
+        alertBtn.tap()
+        
+        XCTAssertTrue(app.staticTexts["Job can't be empty"].exists)
         
         XCTAssertEqual(app.alerts.count, 0, "There should be no alerts on the screen")
     }
